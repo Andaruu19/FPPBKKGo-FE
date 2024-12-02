61bd570c5f4806993cf15cb3dca02a91a7afad52
Andaruu19 <prelhanip@gmail.com>
2024-12-02 20:06:08 +0700

    feat: working movie page without addtoalbum

---
+++ nextfixapp/src/app/movies/[slug]/page.tsx
@@ -0,0 +1,132 @@
+// src/app/movies/[slug]/page.tsx
+"use client";
+
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import axios from "axios";
+import Link from "next/link";
+
+interface Actor {
+  ID: number;
+  Name: string;
+}
+
+interface Genre {
+  ID: number;
+  Name: string;
+}
+
+interface Movie {
+  ID: number;
+  Slug: string;
+  Title: string;
+  Description: string;
+  ImagePath: string;
+  Year: string;
+  GenreID: number;
+  Genre: Genre;
+  ActorID: number;
+  Actor: Actor;
+}
+
+export default function MoviePage() {
+  const params = useParams();
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        const response = await axios.get<Movie>(
+          `http://localhost:8080/movies/${params.slug}`
+        );
+        setMovie(response.data);
+      } catch (error) {
+        console.error("Error fetching movie:", error);
+      }
+    };
+    fetchMovie();
+  }, [params.slug]);
+
+  if (!movie) return <div>Loading...</div>;
+
+  return (
+    <section
+      className="relative h-screen w-full bg-black bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/${movie.ImagePath}.jpg')`,
+      }}
+    >
+      <div className="mx-auto max-w-screen-xl px-4 py-24 text-center lg:py-56">
+        <h1 className="mb-4 text-4xl font-extrabold uppercase leading-none tracking-tight text-white md:text-5xl lg:text-6xl">
+          {movie.Title}
+        </h1>
+        <p className="mb-4 text-lg font-normal text-gray-300 sm:px-16 lg:px-48 lg:text-xl">
+          {movie.Description}
+        </p>
+
+        <div className="mb-4 space-x-2">
+          <span className="inline-flex items-center rounded bg-primary-100 px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:bg-primary-200 dark:text-primary-800">
+            <svg
+              className="mr-1 h-3 w-3"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="m17 21-5-4-5 4V3.889a.92.92 0 0 1 .244-.629.808.808 0 0 1 .59-.26h8.333a.81.81 0 0 1 .589.26.92.92 0 0 1 .244.63V21Z" />
+            </svg>
+            {movie.Genre?.Name}
+          </span>
+
+          <span className="inline-flex items-center rounded bg-green-100 px-2.5 py-0.5 text-xs font-medium text-green-800 dark:bg-green-200 dark:text-green-800">
+            <svg
+              className="mr-1 h-3 w-3"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" />
+            </svg>
+            {movie.Actor?.Name}
+          </span>
+
+          <span className="inline-flex items-center rounded bg-yellow-100 px-2.5 py-0.5 text-xs font-medium text-yellow-800 dark:bg-yellow-200 dark:text-yellow-800">
+            <svg
+              className="mr-1 h-3 w-3"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M5 5a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1 2 2 0 0 1 2 2v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a2 2 0 0 1 2-2Z" />
+            </svg>
+            {movie.Year}
+          </span>
+        </div>
+
+        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+          <Link
+            href="https://www.youtube.com/watch?v=bpHhnDQuFgU"
+            className="inline-flex items-center justify-center rounded-lg bg-red-700 px-5 text-center text-base font-medium text-white hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900"
+          >
+            <svg
+              className="me-2 h-[32px] w-[32px] text-white"
+              aria-hidden="true"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8.6 5.2A1 1 0 0 0 7 6v12a1 1 0 0 0 1.6.8l8-6a1 1 0 0 0 0-1.6l-8-6Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            Play
+          </Link>
+
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="inline-flex items-center justify-center rounded-lg border border-white px-5 py-3 text-center text-base font-medium text-white hover:bg-gray-100 hover:text-gray-900 focus:ring-4 focus:ring-gray-400 sm:ms-4"
+          >
+            Add to album
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
